24cd1a5a79230d978111aef06fa0c82e0a590529
jinhwansong <119153742+jinhwansong@users.noreply.github.com>
2023-03-07 11:04:39 +0900

    style : 마이페이지 이미지 기능 추가중.... #11

--- src/component/mypage/Profile.jsx
+++ src/component/mypage/Profile.jsx
@@ -1,13 +1,48 @@
-import React from 'react'
+import React,{ useState } from 'react'
 import styled from "styled-components"
 import { Arrow } from '../header/style';
+
 function Profile() {
+  //미리보기를 구현하기위한 스테이트
+  const [imgBaseChange, setImgBaseChange] = useState([]);
+  //이미지파일을 받는 스테이트
+  const [imgFile, setImgFile] = useState([]);
+  const imgHandlerChange = (event) => {
+    //console.log(event.target.files);
+    //파일을 받아오는 이벤트
+    //setImgFile(event.target.files);
+    setImgBaseChange([]);
+    for (let i = 0; i < event.target.files.length; i++) {
+      if (event.target.files[i]) {
+        //파일을 버퍼에 저장시켜줌
+        //정확히는 해당 파일 리더를 통해서 파일 정보를 읽는다
+        let reader = new FileReader();
+        //파일 정보를 입력한다.!
+        reader.readAsDataURL(event.target.files[i]);
+        //파일 상태 업데이트
+        reader.onloadend = () => {
+          //파일 읽기가 완료되면 아래코드가 실행된다.
+          //reader.result는 파일을 비트맵 데이터를 리턴시켜준다.
+          const base = reader.result;
+          //console.log(base)
+          if (base) {
+            //비트맵 데이터를 저장 가능하도록 스트링으로 바꾼다.
+            const baseSub = base.toString();
+            //imgBase에 baseSub를 붙인 새로운 배열을 스테이트 한다.
+            setImgBaseChange((imgBaseChange) => [...imgBaseChange, baseSub]);
+          }
+        };
+      }
+    }
+  };
+
   return (
     <ProflieWrap>
       <ProflieInner>
         <Title>프로필 변경</Title>
         <Content>
           <Left>
+            <img src="" alt="" />
             <InputProfile
               type="file"
               id="imagebox"
@@ -32,7 +67,7 @@ function Profile() {
             </LabelProfile>
           </Left>
           <Right>
-            <Input type="text"  />
+            <Input type="text" />
             <Emtitle>언어</Emtitle>
             <SelectBoxWrap>
               <SelectBox>
